4258cdc1030f1a105aaf9e26f873548a03d60dfe
jayp220 <1670seo@naver.com>
2025-09-25 13:12:43 +0900

    Create BOJ_3273(array).cpp

---
+++ 2025-2/Basic/SJP/Array/BOJ_3273(array).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[100000]; //문제에서 주어진 n의 최댓값
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    int x;
+    cin >> x;
+
+    int result = 0;
+    int left = 0;
+    int right = n - 1;
+
+    // 투 포인터로 두 수의 합이 x인지 확인
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum < x) {
+            left++;
+        } else if (sum > x) {
+            right--;
+        } else {
+            result++;
+            left++;
+            right--;
+        }
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
