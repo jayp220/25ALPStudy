c3330f1b84ebff43ef8ec78c97744ecabf74962d
jayp220 <1670seo@naver.com>
2025-09-25 13:14:08 +0900

    Update BOJ_3273(vec).cpp

--- 2025-2/Basic/SJP/Array/BOJ_3273(vec).cpp
+++ 2025-2/Basic/SJP/Array/BOJ_3273(vec).cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int main() {
@@ -37,6 +36,5 @@ int main() {
     }
 
     cout << result << endl;
-
     return 0;
 }
